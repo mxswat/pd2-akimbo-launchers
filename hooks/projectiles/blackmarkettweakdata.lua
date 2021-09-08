23bfad4f26ce3eb4776c5e9183271cb1c9b797de
Massimo 'Mx' Cacciapaglia <massimocacciapaglia.13@gmail.com>
2021-09-08 22:47:28 +0200

    Cleaned unit generation code

--- hooks/projectiles/blackmarkettweakdata.lua
+++ hooks/projectiles/blackmarkettweakdata.lua
@@ -12,7 +12,7 @@ local mysplit = function(inputstr, sep)
 end
 
 -- local _mod_path = Application:base_path()..'assets/mod_overrides/pd2-akimbo-launchers/'
--- Hooks:PostHook(BlackMarketTweakData, "_init_projectiles", "ClientProjectiles_BlackMarketTweakData_init_projectiles", function(self, tweak_data)
+-- Hooks:PostHook(BlackMarketTweakData, "_init_projectiles", "GenerateUnits_BlackMarketTweakData_init_projectiles", function(self, tweak_data)
 --     -- Clean the content of _mod_path..'/Assets/units'
 --     os.execute(string.format('RD /S /Q "%s"', Application:nice_path(_mod_path..'/Assets/units')))
 --     for key, projectile in pairs(self.projectiles) do
@@ -20,12 +20,8 @@ end
 --         local weapon_id = projectile.weapon_id -- Is Associated to a weapon
 --         if projectile_unit and weapon_id and not projectile.local_unit then -- Exclude if it already has a local
 --             local unit_xml = DB:open("unit", projectile_unit):read()
---             projectile.no_cheat_count = true -- Make it client side
---             projectile.client_authoritative = true -- Make it client side
---             projectile.time_cheat = 0 -- Make it client side
 --             local _nus = mysplit(projectile_unit, "/") -- new unit string
 --             local _nu_last = _nus[#_nus] -- Get last from split
---             projectile.local_unit = 'local_units/'.. _nu_last .. '_local' 
 --             _nus[#_nus] = nil -- Remove last item
 --             local _nu_path = string.join('/', _nus) -- Rejoin the string
 --             -- Use the NU_Unit path to create the appropriate folder
@@ -34,27 +30,18 @@ end
 --             log(pd2_proj_unit_path)
 --             local _unit_file = io.open(pd2_proj_unit_path, "w+")
 --             local newXMLContent = unit_xml:gsub('<network sync="spawn" />', '') -- Put the new xml content in the xml, but remove the sync
---             -- local unit_file_os_path = (ModPath..'/Assets/'.. projectile_unit..'.unit')
 --             _unit_file:write(newXMLContent)
 --             _unit_file:close()
---             -- log('projectile_unit:'..projectile_unit)
---             -- log('Path:'..unit_file_os_path)
---             -- log('---Original---')
---             -- log(unit_xml)
---             -- BLT.AssetManager:CreateEntry(Idstring(projectile_unit), Idstring("unit"), unit_file_os_path)
---             -- log('--Mxs--')
---             -- log(DB:open("unit", projectile_unit):read())
 --         end
 --     end
 -- end)
 
-local _mod_path = Application:base_path()..'assets/mod_overrides/pd2-akimbo-launchers/'
 Hooks:PostHook(BlackMarketTweakData, "_init_projectiles", "ClientProjectiles_BlackMarketTweakData_init_projectiles", function(self, tweak_data)
     for key, projectile in pairs(self.projectiles) do
         local projectile_unit = projectile.unit
         local weapon_id = projectile.weapon_id -- Is Associated to a weapon
         if projectile_unit and weapon_id and not projectile.local_unit then -- Exclude if it already has a local
-            local unit_xml = DB:open("unit", projectile_unit):read()
+            -- local unit_xml = DB:open("unit", projectile_unit):read()
             projectile.no_cheat_count = true -- Make it client side
             projectile.client_authoritative = true -- Make it client side
             projectile.time_cheat = 0 -- Make it client side
