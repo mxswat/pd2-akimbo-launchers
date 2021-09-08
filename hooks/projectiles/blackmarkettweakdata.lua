0bf2d0d5def3e928ba4a907b8fb414fe8dea2c4c
Massimo 'Mx' Cacciapaglia <massimocacciapaglia.13@gmail.com>
2021-09-08 18:30:08 +0200

    First iteration of auto write/read of Projectiles

--- hooks/projectiles/blackmarkettweakdata.lua
+++ hooks/projectiles/blackmarkettweakdata.lua
@@ -1,12 +1,33 @@
+-- Credits to DR NEWBIE
+local mysplit = function(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t={} ; i=1
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
+
 Hooks:PostHook(BlackMarketTweakData, "_init_projectiles", "ClientProjectiles_BlackMarketTweakData_init_projectiles",    function(self, tweak_data)
-	-- self.projectiles.launcher_incendiary_arbiter.unit = "client_explosives/arbiter_frag_incendiary"
+	
     local _mod_path = Application:base_path()..'assets/mod_overrides/pd2-akimbo-launchers/'
-    os.execute(string.format('RD /S /Q "%s"', Application:nice_path(_mod_path..'/Assets/')))
+    -- Clean the content of _mod_path..'/Assets/units'
+    os.execute(string.format('RD /S /Q "%s"', Application:nice_path(_mod_path..'/Assets/units')))
+    -- Get the current projectile to iterate
     local projectile_unit = self.projectiles.launcher_incendiary_arbiter.unit
     local unit_xml = DB:open("unit", projectile_unit):read()
-    os.execute('mkdir "'.. Application:nice_path(_mod_path..'/Assets/'.. projectile_unit, true) ..'"')
+    local _nus = mysplit(projectile_unit, "/") -- new unit string
+    local _nu_last = _nus[#_nus] -- Get last from split
+    _nus[#_nus] = nil -- Remove last item
+    local _nu_path = string.join('/', _nus) -- Rejoin the string
+    -- Use the NU_Unit path to create the appropriate folder
+    os.execute('mkdir "'.. Application:nice_path(_mod_path..'/Assets/'.. _nu_path, true) ..'"')
     local _unit_file = io.open('assets/mod_overrides/pd2-akimbo-launchers/Assets/'.. projectile_unit ..'.unit', "w+")
-    local newXMLContent = unit_xml:gsub('<network sync="spawn" />', '')
+    local newXMLContent = unit_xml:gsub('<network sync="spawn" />', '') -- Put the new xml content in the xml, but remove the sync
     _unit_file:write(newXMLContent)
+    _unit_file:close()
 end)
 
